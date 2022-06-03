126f426a31f0eca26316de66f372fcf1df5a1519
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2022-06-03 14:11:59 -0800

    Initial Commit

---
+++ SuperCoolGV/SuperCoolGVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCoolGVApp.swift
+//  SuperCoolGV
+//
+//  Created by Gavin Vasandani on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperCoolGVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
